9e5ad4097b24d1f3bc763ecaa051860a9161b082
FurkanHuman <furkan-b136@hotmail.com>
2023-12-14 03:29:28 +0300

    added and fixed exam page template

---
+++ Business/ExamPage/IPage.cs
@@ -0,0 +1,10 @@
+﻿using Entity.Entities.Mains;
+using QuestPDF.Infrastructure;
+
+namespace App.ExamPage;
+
+public interface IPage
+{
+    IDocument BackPageGenerate(Exam exam);
+    IDocument FrontPageGenerate(Exam exam);
+}
