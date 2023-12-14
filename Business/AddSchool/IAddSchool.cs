17fd77825ba07d2e8dbd395395b01d497edef19a
FurkanHuman <furkan-b136@hotmail.com>
2023-12-14 03:30:23 +0300

    School add console app added

---
+++ Business/AddSchool/IAddSchool.cs
@@ -0,0 +1,13 @@
+﻿using Entity.Entities.Mains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.AddSchool;
+
+public interface IAddSchool
+{
+    School AddSchool(Exam exam);
+}
