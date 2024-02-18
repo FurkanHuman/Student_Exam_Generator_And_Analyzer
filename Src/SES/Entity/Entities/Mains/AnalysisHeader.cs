a55a4883abb6eb05e5ee1ff766c131cbada1b8f2
FurkanHuman <furkan-b136@hotmail.com>
2024-02-18 18:06:17 +0300

    Is it a finale or a sequel?

--- Src/SES/Entity/Entities/Mains/AnalysisHeader.cs
+++ Src/SES/Entity/Entities/Mains/AnalysisHeader.cs
@@ -5,27 +5,27 @@ namespace Entity.Entities.Mains;
 public class AnalysisHeader : Entity<int>
 {
     public int ClassAge { get; set; } // sınıf yaşı 
-    public required char AltClass { get; set; } // sıjnıf şubesi tek hane
+    public required char AltClass { get; set; } // sınıf şubesi tek hane
     public string ExamSemesterYear { get; set; } // note: sınav dönemi Yılı. örnek 2000 - 2001
     public string LessonName { get; set; } // note: sınav adım
     public string Semester { get; set; } // note: dönem . örnek 1. Dönem 2. Dönem...
     public string SemesterSession { get; set; } // note: bir dönemdeki sınavlar. örnek 1. sınav 2. sınav...
 
-    public int BenefitId { get; set; }
-    public int[] RefScorePerQuestions { get; set; }
-
     public string ExamCode { get; set; }
     public string FooterNote { get; set; }
 
+    public int BenefitId { get; set; }
+    public int QuestionId { get; set; }
     public int TeacherId { get; set; }
     public int PrincipalId { get; set; }
     public int SqaId { get; set; }
     public int SchoolId { get; set; }
 
-
     public Teacher Teacher { get; set; }
     public Principal Principal { get; set; }
     public School School { get; set; }
+    public int[] RefScorePerQuestions { get; set; }
+    public IList<QuizQuestion> Questions { get; set; }
     public IList<Benefit> Benefits { get; set; }
     public IList<StudentQuizAnswer> StudentQuizAnswers { get; set; }
 }
