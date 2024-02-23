9708d4ebe0395ecec89ccf4d75b6e1adf8222cd1
FurkanHuman <furkan-b136@hotmail.com>
2024-02-23 17:10:16 +0300

    just migration

---
+++ Src/SES/MigApi/Program.cs
@@ -0,0 +1,33 @@
+using App;
+using Persistence;
+using NArchitecture.Core.Persistence.WebApi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAppServiceRegistration();
+builder.Services.AddPersistenceServices();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseDbMigrationApplier();
+
+app.MapControllers();
+
+app.Run();
