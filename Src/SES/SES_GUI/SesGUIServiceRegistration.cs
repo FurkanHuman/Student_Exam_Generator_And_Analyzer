7e0ebe31dfc5463c487b114cfbe24242fc333b3d
FurkanHuman <furkan-b136@hotmail.com>
2024-02-24 03:25:51 +0300

    update service registration and packs

--- Src/SES/SES_GUI/SesGUIServiceRegistration.cs
+++ Src/SES/SES_GUI/SesGUIServiceRegistration.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+using App;
 
 namespace SES_GUI;
 
@@ -6,6 +8,8 @@ public static class SesGUIServiceRegistration
 {
     public static IServiceCollection AddSesGUIServiceRegistration(this IServiceCollection services)
     {
+        services.AddAppServiceRegistration();
+        services.AddPersistenceServices();
 
         services.AddTransient<SES_Main>();
         services.AddTransient<Analysis_Full>();
