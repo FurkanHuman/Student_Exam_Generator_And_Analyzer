0f0f64e5459cb03eb9cb069134f8bf557c4f526d
FurkanHuman <furkan-b136@hotmail.com>
2024-06-08 20:23:38 +0300

    feat(domain): user and teacher one2one

--- Src/SES/Domain/Entities/Teacher.cs
+++ Src/SES/Domain/Entities/Teacher.cs
@@ -11,8 +11,10 @@ public class Teacher : Entity<int>
     public int ExamId { get; set; }
     public int StudentId { get; set; }
     public int SchoolId { get; set; }
-    public School School { get; set; }
+    public int UserId { get; set; }
 
+    public User User { get; set; }
+    public School School { get; set; }
     public Semester Semester { get; set; }
     public IList<ReferenceBenefit> ReferenceBenefits { get; set; }
     public IList<Exam> Exams { get; set; }
