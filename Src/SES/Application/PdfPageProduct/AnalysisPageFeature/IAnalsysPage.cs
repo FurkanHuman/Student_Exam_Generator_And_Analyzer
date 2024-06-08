305650950a87febdeb9499d48a55b4a8e6519a32
FurkanHuman <furkan-b136@hotmail.com>
2024-06-08 18:29:33 +0300

    feat(pdfPageProduct,): deleted for later use

--- Src/SES/Application/PdfPageProduct/AnalysisPageFeature/IAnalsysPage.cs
+++
@@ -1,9 +0,0 @@
-﻿using Entity.Entities.Mains;
-using QuestPDF.Infrastructure;
-
-namespace Application.PdfPageProduct.AnalysisPageFeature;
-
-public interface IAnalsysPage
-{
-    IDocument PageGenerate(Analysis analysisHeader);
-}
--- Src/SES/Application/PdfPageProduct/ExamPageFeature/IExamPage.cs
+++
@@ -1,9 +0,0 @@
-﻿using Entity.Entities.Mains;
-using QuestPDF.Infrastructure;
-
-namespace Application.PdfPageProduct.ExamPageFeature;
-
-public interface IExamPage
-{
-    IDocument PageGenerate(Exam exam);
-}
