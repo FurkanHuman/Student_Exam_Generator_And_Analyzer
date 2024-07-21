6af70b1c7a1debf7c629bb55f871290dc05a732b
FurkanHuman <furkan-b136@hotmail.com>
2024-07-21 14:54:40 +0300

    feat(Domain.Entities): added new Db type, entity optimization and build relationship

---
+++ Src/SES/Domain/Entities/Lesson.cs
@@ -0,0 +1,15 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Lesson : Entity<int>
+{
+    public string LessonName { get; set; }
+    public string Description { get; set; }
+    public int SemesterId { get; set; }
+
+    public Semester Semester { get; set; }
+    public IList<StudentClass> StudentClasses { get; set; }
+    public IList<ReferenceBenefit> ReferenceBenefits { get; set; }
+    public IList<Teacher> Teachers { get; set; }
+}
---
+++ Src/SES/Domain/Entities/Personel.cs
@@ -0,0 +1,11 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+public class Personel:Entity<Guid>
+{
+    public Guid UserId { get; set; }
+    public string Name { get; set; }
+    public string SurName { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public virtual User User { get; set; }
+}
