67522a88164712593179442a7663600de83b83ef
FurkanHuman <furkan-b136@hotmail.com>
2024-07-21 18:58:43 +0300

    fix(BlazorWebUI): Type switching has been made

--- Src/SES/BlazorWebUI/BlazorWebUI/CustomAuthenticationStateProvider.cs
+++ Src/SES/BlazorWebUI/BlazorWebUI/CustomAuthenticationStateProvider.cs
@@ -2,7 +2,6 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Application.Services.AuthService;
-using Microsoft.AspNetCore.Http;
 
 namespace BlazorWebUI;
 
@@ -23,12 +22,11 @@ public class CustomAuthenticationStateProvider : AuthenticationStateProvider
 
         JwtSecurityToken jwt = new JwtSecurityTokenHandler().ReadJwtToken(token);
 
-        IEnumerable<Claim> claims = jwt.Claims.Where(c => c.Type == "http://schemas.microsoft.com/ws/2008/06/identity/claims/role");
+        IEnumerable<Claim> claims = jwt.Claims.Where(c => c.Type == ClaimTypes.Role);
 
         ClaimsPrincipal user = new ClaimsPrincipal(new ClaimsIdentity(claims, "Bearer"));
 
         await _authService.AddUserToAuthPipeline(user);
-
         return new AuthenticationState(user);
     }
 
@@ -37,8 +35,8 @@ public class CustomAuthenticationStateProvider : AuthenticationStateProvider
     public void NotifyUserLogout()
     {
         _authService.Logout();
-        var identity = new ClaimsIdentity();
-        var user = new ClaimsPrincipal(identity);
+        ClaimsIdentity identity = new ClaimsIdentity();
+        ClaimsPrincipal user = new ClaimsPrincipal(identity);
         NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
     }
 }
