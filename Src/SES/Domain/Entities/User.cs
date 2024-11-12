9cf986b8f46058ebefbe6e77354cc9b6baf22dd9
FurkanHuman <furkan-b136@hotmail.com>
2024-11-13 01:36:54 +0300

    fix(Domain): user personel optimization

--- Src/SES/Domain/Entities/User.cs
+++ Src/SES/Domain/Entities/User.cs
@@ -2,7 +2,6 @@
 
 public class User : NArchitecture.Core.Security.Entities.User<Guid>
 {
-    public Guid PersonelId { get; init; }
     public virtual Personel Personel { get; set; }
     public virtual ICollection<UserOperationClaim> UserOperationClaims { get; set; } = default!;
     public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = default!;
