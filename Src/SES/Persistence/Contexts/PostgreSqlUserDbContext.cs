f0d124299e70de21180219c03e105c5b1126affb
FurkanHuman <furkan-b136@hotmail.com>
2025-01-02 17:05:34 +0300

    refactor(Persistence): DbContext classes and add PostgreSqlUserDbContext
    Refactored BaseDbContext to use constructor parameters for DbContextOptions and IConfiguration. Updated DbSet properties and expanded OnModelCreating for specific configurations and default schema.
    
    PostgreSqlDbContext now inherits from BaseDbContext and overrides OnModelCreating to set a PostgreSQL-specific schema.
    
    Introduced PostgreSqlUserDbContext inheriting from IdentityDbContext with ApplicationUser and IdentityRole<Guid>, including identity configurations and default schema.
    
    Chain of identity changes in the Persistence 1/4.

---
+++ Src/SES/Persistence/Contexts/PostgreSqlUserDbContext.cs
@@ -0,0 +1,27 @@
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Persistence.EntityConfigurations;
+
+namespace Persistence.Contexts
+{
+    public class PostgreSqlUserDbContext(DbContextOptions<PostgreSqlUserDbContext> options, IConfiguration configuration) : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
+    {
+        protected IConfiguration? Configuration { get; set; } = configuration;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new ApplicationUserConfiguration());
+            builder.ApplyConfiguration(new IdentityRoleConfiguration());
+            builder.ApplyConfiguration(new IdentityUserLoginConfiguration());
+            builder.ApplyConfiguration(new IdentityUserRoleConfiguration());
+            builder.ApplyConfiguration(new IdentityUserTokenConfiguration());
+
+            builder.HasDefaultSchema("SES_Microsoft.AspNetCore.Identity.EntityFrameworkCore");
+            base.OnModelCreating(builder);
+        }
+
+    }
+}
