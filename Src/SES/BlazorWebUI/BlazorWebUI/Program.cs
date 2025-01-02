9da22fde72817538a3b7f482238c409ce8c480f9
FurkanHuman <furkan-b136@hotmail.com>
2025-01-02 17:59:08 +0300

    (feat, reafactor: (BlazorWebUI, BlazorWebUI.Client)): Add authentication and authorization features
    Introduced authentication and authorization to the Blazor WebAssembly app. Added package references and services for identity management. Created new components for handling authentication and user information display. Updated project configurations to support these features. Removed obsolete components and configurations.
    
    - Added package references in BlazorWebUI.Client.csproj and BlazorWebUI.csproj.
    - Created Auth.razor and RedirectToLogin.razor for authentication.
    - Added PersistentAuthenticationStateProvider.cs for managing authentication state.
    - Updated Program.cs to register new services and configurations.
    - Removed Counter.razor, BlazorConfiguration.cs, and CustomAuthenticationStateProvider.cs.
    - Updated appsettings.json and appsettings.Development.json for new configurations.
    - Added libman.json for library management.
    
    Chain of identity changes in the BlazorWebUI, BlazorWebUI.Client 1/2.

--- Src/SES/BlazorWebUI/BlazorWebUI/Program.cs
+++ Src/SES/BlazorWebUI/BlazorWebUI/Program.cs
@@ -1,18 +1,17 @@
 using Application;
 using BlazorWebUI;
+using BlazorWebUI.Client;
 using BlazorWebUI.Components;
+using Domain.Entities;
 using Infrastructure;
-using Microsoft.AspNetCore.Antiforgery;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Identity;
 using NArchitecture.Core.CrossCuttingConcerns.Logging.Configurations;
 using NArchitecture.Core.ElasticSearch.Models;
 using NArchitecture.Core.Localization.WebApi;
 using NArchitecture.Core.Mailing;
 using NArchitecture.Core.Persistence.WebApi;
-using NArchitecture.Core.Security.Encryption;
-using NArchitecture.Core.Security.JWT;
 using Persistence;
+using Persistence.Contexts;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +19,31 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
+
+builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = IdentityConstants.ApplicationScheme;
+    options.DefaultSignInScheme = IdentityConstants.ExternalScheme;
+})
+    .AddIdentityCookies(options =>
+    {
+        options.ApplicationCookie?.Configure(cookieOptions =>
+        {
+            cookieOptions.Cookie.Name = "SES_Credential";
+        });
+    });
+
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole<Guid>>()
+    .AddEntityFrameworkStores<PostgreSqlUserDbContext>()
+    .AddSignInManager()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddApplicationServices(
     mailSettings: builder.Configuration.GetSection("MailSettings").Get<MailSettings>()
         ?? throw new InvalidOperationException("MailSettings section cannot found in configuration."),
@@ -28,48 +52,13 @@ builder.Services.AddApplicationServices(
         .Get<FileLogConfiguration>()
         ?? throw new InvalidOperationException("FileLogConfiguration section cannot found in configuration."),
     elasticSearchConfig: builder.Configuration.GetSection("ElasticSearchConfig").Get<ElasticSearchConfig>()
-        ?? throw new InvalidOperationException("ElasticSearchConfig section cannot found in configuration."),
-    tokenOptions: builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>()
-        ?? throw new InvalidOperationException("TokenOptions section cannot found in configuration.")
+        ?? throw new InvalidOperationException("ElasticSearchConfig section cannot found in configuration.")
 );
 builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddInfrastructureServices();
 builder.Services.AddBlazorWebUIServiceRegistration();
-builder.Services.AddHttpContextAccessor();
 
-const string tokenOptionsConfigurationSection = "TokenOptions";
-TokenOptions tokenOptions =
-    builder.Configuration.GetSection(tokenOptionsConfigurationSection).Get<TokenOptions>()
-    ?? throw new InvalidOperationException($"\"{tokenOptionsConfigurationSection}\" section cannot found in configuration.");
-builder
-    .Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidIssuer = tokenOptions.Issuer,
-            ValidAudience = tokenOptions.Audience,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
-        };
-    });
-
-builder.Services.AddAuthorization();
-builder.Services.AddAuthorizationCore();
-builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddDistributedMemoryCache();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddCors(opt =>
-    opt.AddDefaultPolicy(p =>
-    {
-        p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-    })
-);
-
 
 WebApplication app = builder.Build();
 
@@ -88,9 +77,6 @@ else
 
 app.UseDbMigrationApplier();
 
-app.UseAuthentication();
-app.UseAuthorization();
-
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
@@ -102,13 +88,7 @@ app.MapRazorComponents<App>()
     .AddAdditionalAssemblies(typeof(BlazorWebUI.Client._Imports).Assembly)
     .DisableAntiforgery();
 
-const string webApiConfigurationSection = "BlazorConfiguration";
-BlazorConfiguration webApiConfiguration =
-    app.Configuration.GetSection(webApiConfigurationSection).Get<BlazorConfiguration>()
-    ?? throw new InvalidOperationException($"\"{webApiConfigurationSection}\" section cannot found in configuration.");
-app.UseCors(opt => opt.WithOrigins(webApiConfiguration.AllowedOrigins).AllowAnyHeader().AllowAnyMethod().AllowCredentials());
 
 app.UseResponseLocalization();
-
-
-await app.RunAsync();
+app.MapAdditionalIdentityEndpoints();
+app.Run();
