811b60e7d33297a9c49cd4d2c2c1a0d483752540
FurkanHuman <furkan-b136@hotmail.com>
2025-02-06 20:41:11 +0300

    feat(BlazorWebUI, BlazorWebUI.Client): added new page and components. Refactor and add new components for quiz and benefits
    Refactored CheckBoxList to be generic and removed old CheckboxListBasic component. Added new components: QQBenefitAdd, QQOption, QQOptionBody, QQBenefitLoader, and QuizPoolAdd. Updated QQBody with new input fields and logic. Introduced new data classes: FileData, QQBodyDto, and QuestionOptionDto. Updated various components to use these new data structures.

---
+++ Src/SES/BlazorWebUI/BlazorWebUI.Client/Pages/Styles/QQ/QQBodyDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorWebUI.Client.Pages.Styles.QQ;
+
+public class QQBodyDto
+{
+    public string QuestionBody { get; set; } = string.Empty;
+    public string? Question { get; set; }
+    public byte SelectedQType { get; set; }
+    public int Score { get; set; }
+    public int MaxScore { get; set; }
+    public FileData? FileData { get; set; }
+    public ICollection<int> SelectedBenefitIds { get; set; } = [];
+    public ICollection<QuestionOptionDto> QuestionOptions { get; set; } = [];
+}
---
+++ Src/SES/BlazorWebUI/BlazorWebUI.Client/Pages/Styles/QQ/QuestionOptionDto.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorWebUI.Client.Pages.Styles.QQ;
+
+public class QuestionOptionDto
+{
+    public bool IsCorrect { get; set; }
+    public string? OptionText { get; set; }
+}
