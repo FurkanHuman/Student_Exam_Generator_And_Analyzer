071aab32fee4415f218394431e8ccebb9dc2d568
FurkanHuman <furkan-b136@hotmail.com>
2025-02-06 21:03:22 +0300

    refactor(Persistence): Update relationships in configuration classes
    - `LessonConfiguration`: Added relationships with `StudentClasses`, `Exams`, and `QuizQuestions`.
    - `QuizQuestionConfiguration`: Made `QuestionBody` and `QuestionImageURL` optional; added relationship with `Lessons`.
    - `ReferenceBenefitConfiguration`: Removed relationship with `QuizQuestions`.

--- Src/SES/Persistence/EntityConfigurations/LessonConfiguration.cs
+++ Src/SES/Persistence/EntityConfigurations/LessonConfiguration.cs
@@ -21,6 +21,8 @@ public class LessonConfiguration : IEntityTypeConfiguration<Lesson>, IMainConfig
         builder.HasMany(l => l.StudentClasses);
         builder.HasMany(l => l.ReferenceBenefits);
         builder.HasMany(l => l.Teachers);
+        builder.HasMany(l => l.Exams);
+        builder.HasMany(l => l.QuizQuestions);
 
         builder.Property(l => l.CreatedDate).IsRequired();
         builder.Property(l => l.UpdatedDate);
