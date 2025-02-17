58108dbba9d6cf8c9ced65c66b2182e811eb166c
FurkanHuman <furkan-b136@hotmail.com>
2025-02-17 17:44:55 +0300

    style(Domain): removed extra spaces

--- Src/SES/Domain/Entities/Lesson.cs
+++ Src/SES/Domain/Entities/Lesson.cs
@@ -7,7 +7,6 @@ public class Lesson : Entity<int>
     public string LessonName { get; set; }
     public string Description { get; set; }
     public int SemesterId { get; set; }
-
     public Semester Semester { get; set; }
     public IList<StudentClass> StudentClasses { get; set; }
     public IList<ReferenceBenefit> ReferenceBenefits { get; set; }
