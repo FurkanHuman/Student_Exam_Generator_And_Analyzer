d6c395334f7fa51a905dc8489d36bb68efc9c37d
FurkanHuman <furkan-b136@hotmail.com>
2025-02-21 17:29:47 +0300

    feat(BlazorWebUI.Client): Add view mode support and refactor option management
    - Introduced `IsViewMode` parameter in `QQOption.razor` to conditionally enable/disable UI elements.
    - Centralized default option values in a new static class `QQStatic`.
    - Updated `QQOptionBody.razor` to utilize `QQStatic.SelectTypeOptions` for radio button options.
    - Enhanced `QQView.razor` to display question type, score, and details.
    - Improved code maintainability and organization through these changes.

---
+++ Src/SES/BlazorWebUI/BlazorWebUI.Client/Pages/Styles/QQ/QQStatic.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorWebUI.Client.Pages.Styles.QQ;
+
+public static class QQStatic
+{
+    public static IDictionary<byte, string> SelectTypeOptions { get; set; } = new Dictionary<byte, string>
+    {
+        {1, "Açık Uçlu" },
+        // { 2, "Kapalı Uçlu" },
+        { 3, "Çoktan Seçmeli" },
+        { 4, "Doğru/Yanlış" },
+        { 5, "Boşluk Doldurma" },
+        // { 6, "Eşleştirme" },
+        // { 7, "Sıralama"  }
+    };
+
+    public static readonly int DefaultMultipleChoiceOptions = 4;
+    public static readonly int DefaultTrueFalseOptions = 2;
+    public static readonly int DefaultFillInTheBlankOptions = 1;
+
+
+}
