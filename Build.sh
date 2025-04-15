626a7d4d0c3be1acf630d7898b5949f1f9c9eab8
FurkanHuman <furkan-b136@hotmail.com>
2025-04-15 16:45:29 +0300

    fix(/): Uncomment docker rm command to ensure container removal

--- Build.sh
+++ Build.sh
@@ -25,7 +25,7 @@ RUNNING_CONTAINER=$(docker ps -q --filter "name=$APP_NAME")
 if [ ! -z "$RUNNING_CONTAINER" ]; then
     echo "Running container found for $APP_NAME. Stopping it..."
     docker stop "$RUNNING_CONTAINER"
-#    docker rm "$RUNNING_CONTAINER" # Uncomment if you want to remove the container
+    docker rm "$RUNNING_CONTAINER"
 else
     echo "No running container found for $APP_NAME."
 fi
