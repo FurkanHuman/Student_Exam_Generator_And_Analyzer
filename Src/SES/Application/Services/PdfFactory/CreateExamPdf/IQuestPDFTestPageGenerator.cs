64701b6adca12423bb55daed91f19b1933191606
FurkanHuman <furkan-b136@hotmail.com>
2025-04-29 04:23:52 +0300

    feat(Application): Add IQuestPDFTestPageGenerator interface and implementation
    Introduces a new interface `IQuestPDFTestPageGenerator` with a method `PageGenerate(Exam exam)` in `IQuestPDFTestPageGenerator.cs`. Updates `ExamPageRenderer` to implement this interface alongside `IExamPageGenerator`. Adds a specific implementation of `PageGenerate` for handling PDF generation based on quiz questions, while retaining the existing method for production use, enhancing separation of concerns for testing purposes.

---
+++ Src/SES/Application/Services/PdfFactory/CreateExamPdf/IQuestPDFTestPageGenerator.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+using QuestPDF.Infrastructure;
+
+namespace Application.Services.PdfFactory.CreateExamPdf;
+
+public interface IQuestPDFTestPageGenerator
+{
+    IDocument PageGenerate(Exam exam);
+}
