55b42dc8dfa06d2a553d3098af08c207372476e4
FurkanHuman <furkan-b136@hotmail.com>
2025-05-03 02:20:20 +0300

    feat(BlazorWebUI): Add file download functionality with download.js
    - Updated `App.razor` to include a script for `download.js`.
    - Implemented `downloadFileFromStream` function in `download.js` to enable downloading files from base64 data.

---
+++ Src/SES/BlazorWebUI/BlazorWebUI/wwwroot/js/download.js
@@ -0,0 +1,8 @@
+﻿window.downloadFileFromStream = async (filename, base64Data, mimeType) => {
+    const link = document.createElement('a');
+    link.download = filename;
+    link.href = `data:${mimeType};base64,${base64Data}`;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
