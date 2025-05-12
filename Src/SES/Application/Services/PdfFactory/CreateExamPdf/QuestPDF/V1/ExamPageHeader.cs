397e38ff59624af636cc9259d4939228409aa0bd
FurkanHuman <furkan-b136@hotmail.com>
2025-05-12 20:52:53 +0300

    fix(Application): Correct formatting of lesson and class information in exam header

--- Src/SES/Application/Services/PdfFactory/CreateExamPdf/QuestPDF/V1/ExamPageHeader.cs
+++ Src/SES/Application/Services/PdfFactory/CreateExamPdf/QuestPDF/V1/ExamPageHeader.cs
@@ -48,8 +48,8 @@ internal static class ExamPageHeader
             {
                 generalInfo.Item().AlignCenter().Text($"{exam.Semester.BeginSemesterDate.Year} - {exam.Semester.EndSemesterDate.Year}").Bold();
                 generalInfo.Item().AlignCenter().Text(exam.School.Name).Bold();
-                generalInfo.Item().AlignCenter().Text($"{exam.Lesson.LessonName} {ExamConstants.Get("lesson")}").Bold();
-                generalInfo.Item().AlignCenter().Text($"{examInfo.SelectedClass}. {ExamConstants.Get("clasess")} {examInfo.ExamTerm}. {ExamConstants.Get("period")} {examInfo.CurrentExamNumber}. {ExamConstants.Get("written_exam")}").Bold();
+                generalInfo.Item().AlignCenter().Text($"{exam.Lesson.LessonName}{ExamConstants.Get("lesson")}").Bold();
+                generalInfo.Item().AlignCenter().Text($"{examInfo.SelectedClass}. {ExamConstants.Get("clasess")} {exam.Semester.Name}. {ExamConstants.Get("period")} {examInfo.CurrentExamNumber}. {ExamConstants.Get("written_exam")}").Bold();
             });
 
             headerRow.RelativeItem(1).AlignRight().AlignTop().Column(examSummary =>
