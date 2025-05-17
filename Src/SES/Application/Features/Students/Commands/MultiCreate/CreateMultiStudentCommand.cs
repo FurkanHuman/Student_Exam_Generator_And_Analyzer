849f91060ac9b76522087538cfb67ec486256bb6
FurkanHuman <furkan-b136@hotmail.com>
2025-05-17 23:39:43 +0300

    refactor(Application): Move CreateMultiStudentCommandHandler class
    Relocate the CreateMultiStudentCommandHandler from its own file into CreateMultiStudentCommand.cs.
    Add necessary using directives and implement the handler logic for processing multiple student creation from a PDF file.
    Dependencies for mapping, student repository, student class service, PDF reading service, and business rules are included.

--- Src/SES/Application/Features/Students/Commands/MultiCreate/CreateMultiStudentCommand.cs
+++ Src/SES/Application/Features/Students/Commands/MultiCreate/CreateMultiStudentCommand.cs
@@ -1,4 +1,11 @@
 ﻿using Application.Features.Students.Constants;
+using Application.Features.Students.Rules;
+using Application.Services.PdfFactory.PdfReaderService;
+using Application.Services.PdfFactory.PdfReaderService.Dtos;
+using Application.Services.Repositories;
+using Application.Services.StudentClasses;
+using AutoMapper;
+using Domain.Entities;
 using MediatR;
 using NArchitecture.Core.Application.Pipelines.Authorization;
 using NArchitecture.Core.Application.Pipelines.Caching;
@@ -14,12 +21,65 @@ public class CreateMultiStudentCommand : IRequest<ICollection<CreatedMultiStuden
     public required int RefTeacherId { get; set; }
     public required byte[] PdfFile { get; set; }
 
-    
-
     public bool BypassCache { get; }
 
     public string? CacheKey { get; }
 
     public string[]? CacheGroupKey => ["GetStudents"];
 
+    public class CreateMultiStudentCommandHandler : IRequestHandler<CreateMultiStudentCommand, ICollection<CreatedMultiStudentResponse>>
+    {
+        private readonly IMapper _mapper;
+        private readonly IStudentRepository _studentRepository;
+        private readonly IStudentClassService _studentClassService;
+        private readonly IPdfReaderService _pdfReaderService;
+        private readonly StudentBusinessRules _studentBusinessRules;
+
+        public CreateMultiStudentCommandHandler(IMapper mapper, IStudentRepository studentRepository, IStudentClassService studentClassService, IPdfReaderService pdfReaderService, StudentBusinessRules studentBusinessRules)
+        {
+            _mapper = mapper;
+            _studentRepository = studentRepository;
+            _studentClassService = studentClassService;
+            _pdfReaderService = pdfReaderService;
+            _studentBusinessRules = studentBusinessRules;
+        }
+
+        public async Task<ICollection<CreatedMultiStudentResponse>> Handle(CreateMultiStudentCommand request, CancellationToken cancellationToken)
+        {
+            ICollection<ClassWithStudentsDto> extractStudentAndClasess = await _pdfReaderService.GetAllClassesAndStudents(request.PdfFile);
+            ICollection<Student> createStudents = [];
+
+            foreach (ClassWithStudentsDto classWithStudents in extractStudentAndClasess)
+            {
+
+                StudentClass autocreatedStudentClass = await _studentClassService.AddAsync(new()
+                {
+                    Name = "Auto Added",
+                    ClassAge = classWithStudents.Age,
+                    ClassBranch = classWithStudents.Section,
+                    SchoolId = request.SchoolId,
+                    SemesterId = request.SemesterId,
+                    RefTeacherId = request.RefTeacherId
+                });
+
+                foreach (string[] sArray in classWithStudents.Students)
+                {
+                    createStudents.Add(new()
+                    {
+                        SchoolNumber = sArray[0],
+                        Name = sArray[1],
+                        SurName = sArray[2],
+                        Gender = sArray[3][0],
+                        SchoolId = autocreatedStudentClass.SchoolId,
+                        StudentClassId = autocreatedStudentClass.Id,
+                        Description = "Auto Added"
+                    });
+                }
+            }
+
+            ICollection<Student> addedStudents = await _studentRepository.AddRangeAsync(createStudents, cancellationToken);
+
+            return _mapper.Map<ICollection<CreatedMultiStudentResponse>>(addedStudents);
+        }
+    }
 }
