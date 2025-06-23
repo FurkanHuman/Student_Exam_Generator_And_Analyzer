4fc0417308d89c83bc33702a74b9bea4e45ec239
FurkanHuman <furkan-b136@hotmail.com>
2025-06-23 13:05:44 +0300

    feat(QQStatic): Add Turkish options and utility methods in QQStatic
    - Introduced `SelectTypeOptionsTUR` for Turkish question types.
    - Added `ShuffleStringArray` method for shuffling string arrays.
    - Created `Alphabet` array containing uppercase letters A-Z.
    - Defined default values for various question types.

--- Src/SES/BlazorWebUI/BlazorWebUI.Client/Pages/Styles/QQ/QQStatic.cs
+++ Src/SES/BlazorWebUI/BlazorWebUI.Client/Pages/Styles/QQ/QQStatic.cs
@@ -15,7 +15,7 @@ public static class QQStatic
         { 6, "Onaylı intihal" }
     };
 
-    public static readonly IDictionary<byte, string> SelectTypeOptions = new Dictionary<byte, string>
+    public static readonly IDictionary<byte, string> SelectTypeOptionsTUR = new Dictionary<byte, string>
     {
         {1, "Açık Uçlu" },
         // { 2, "Kapalı Uçlu" },
@@ -26,6 +26,18 @@ public static class QQStatic
         // { 7, "Sıralama"  }
     };
 
+    // Shuffles a string array using the Fisher-Yates algorithm
+    internal static void ShuffleStringArray(ref string[] array, int seed)
+    {
+        Random rng = new(seed);
+        for (int i = array.Length - 1; i > 0; i--)
+        {
+            int j = rng.Next(i + 1);
+            (array[i], array[j]) = (array[j], array[i]);
+        }
+    }
+
+    public static readonly char[] Alphabet = [.. Enumerable.Range('A', 26).Select(c => (char)c)];
     public static readonly int DefaultOpenEndedOptions = 1;
     public static readonly int DefaultMultipleChoiceOptions = 4;
     public static readonly int DefaultTrueFalseOptions = 2;
