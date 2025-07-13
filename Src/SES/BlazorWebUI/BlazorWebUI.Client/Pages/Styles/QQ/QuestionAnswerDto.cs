6ff35b56522423baab7717c2de8f1b1d1fd8fcf8
FurkanHuman <furkan-b136@hotmail.com>
2025-07-13 18:54:41 +0300

    feat(QuestionAnswerDto): added bag DTO

---
+++ Src/SES/BlazorWebUI/BlazorWebUI.Client/Pages/Styles/QQ/QuestionAnswerDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorWebUI.Client.Pages.Styles.QQ;
+
+public class QuestionAnswerDto
+{
+    public int StudentId { get; set; }
+    public int ExamId { get; set; }
+    public int QuizQuestionId { get; set; }
+    public Guid? QuestionOptionId { get; set; }
+    public string? AnswerText { get; set; }
+    public int? GivenScore { get; set; }
+    public byte EvaluationOrigin { get; set; }
+    public byte EvaluationStatus { get; set; }
+}
