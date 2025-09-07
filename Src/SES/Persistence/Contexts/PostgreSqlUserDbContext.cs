21d3ae0fdf49130213a259fe9dc81f017b6f7673
FurkanHuman <furkan-b136@hotmail.com>
2025-09-08 00:13:52 +0300

    refactor(PostgreSqlUserDbContext): improve code formatting and structure for better readability

--- Src/SES/Persistence/Contexts/PostgreSqlUserDbContext.cs
+++ Src/SES/Persistence/Contexts/PostgreSqlUserDbContext.cs
@@ -5,19 +5,18 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Persistence.EntityConfigurations.Interfaces;
 
-namespace Persistence.Contexts
+namespace Persistence.Contexts;
+
+public class PostgreSqlUserDbContext(DbContextOptions<PostgreSqlUserDbContext> options, IConfiguration configuration) : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
 {
-    public class PostgreSqlUserDbContext(DbContextOptions<PostgreSqlUserDbContext> options, IConfiguration configuration) : IdentityDbContext<ApplicationUser, IdentityRole<Guid>, Guid>(options)
+    protected IConfiguration? Configuration { get; set; } = configuration;
+
+    protected override void OnModelCreating(ModelBuilder builder)
     {
-        protected IConfiguration? Configuration { get; set; } = configuration;
+        builder.ApplyConfigurationsWithInterface<IUserConfiguration>();
 
-        protected override void OnModelCreating(ModelBuilder builder)
-        {
-            builder.ApplyConfigurationsWithInterface<IUserConfiguration>();
-
-            builder.HasDefaultSchema("SES_Microsoft.AspNetCore.Identity.EntityFrameworkCore");
-            base.OnModelCreating(builder);
-        }
-
+        builder.HasDefaultSchema("SES_Microsoft.AspNetCore.Identity.EntityFrameworkCore");
+        base.OnModelCreating(builder);
     }
+
 }
