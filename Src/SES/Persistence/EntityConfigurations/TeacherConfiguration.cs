c32e626f0bd3ade6dcce2160243b6bae943c764d
FurkanHuman <furkan-b136@hotmail.com>
2025-09-10 01:46:15 +0300

    refactor(TeacherConfiguration): remove redundant StudentAnswers relationship

--- Src/SES/Persistence/EntityConfigurations/TeacherConfiguration.cs
+++ Src/SES/Persistence/EntityConfigurations/TeacherConfiguration.cs
@@ -23,7 +23,6 @@ internal class TeacherConfiguration : IEntityTypeConfiguration<Teacher>, IMainCo
         builder.HasMany(t => t.ReferenceBenefits);
         builder.HasMany(t => t.Exams);
         builder.HasMany(t => t.ExamAuthors).WithOne(e => e.ExamAuthor);
-        builder.HasMany(t => t.StudentAnswers);
         builder.HasMany(t => t.Students);
         builder.HasMany(t => t.Lessons);
         builder.HasMany(t => t.StudentExamAnswers);
