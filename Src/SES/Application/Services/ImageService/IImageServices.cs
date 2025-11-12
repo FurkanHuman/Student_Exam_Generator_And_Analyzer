0499ce8042b1aec783ca243d754ae3b1d54a7539
FurkanHuman <furkan-b136@hotmail.com>
2025-11-12 16:01:09 +0300

    feat(IImageServices): Image Service Added.

---
+++ Src/SES/Application/Services/ImageService/IImageServices.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Services.ImageService
+{
+    public interface IImageServices
+    {
+        Task<Dictionary<string, object>> UploadFileAsync(FileStream fileStream, CancellationToken cancellationToken);
+        Task<Dictionary<string, object>> UploadFileAsync(byte[] fileBytes, string fileName, CancellationToken cancellationToken);
+        Task<(byte[] FileBytes, Dictionary<string, object> Meta)> DownloadFileAsync(string id, CancellationToken cancellationToken);
+        Task<Dictionary<string, object>> DeleteFileAsync(string id, CancellationToken cancellationToken);
+        Task<Dictionary<string, object>> GetFileMetaAsync(string id, CancellationToken cancellationToken);
+        Task<Dictionary<string, object>> GetHealthAsync(CancellationToken cancellationToken);
+    }
+}
