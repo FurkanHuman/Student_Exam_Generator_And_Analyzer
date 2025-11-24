8bbbd532a6f020c8b040d804d2167d7b943e581a
FurkanHuman <furkan-b136@hotmail.com>
2025-11-24 04:05:59 +0300

    fix(Analysis): remove default passing score assignment and adjust passing score handling in AnalysisDetail components

--- Src/SES/Domain/Entities/Lesson.cs
+++ Src/SES/Domain/Entities/Lesson.cs
@@ -6,7 +6,7 @@ public class Lesson : Entity<int>
 {
     public string LessonName { get; set; }
     public string Description { get; set; }
-    public int PassingScore { get; set; } = 50;
+    public int PassingScore { get; set; }
     public bool IsMandatory { get; set; } = false;
     public int SemesterId { get; set; }
     public Semester Semester { get; set; }
