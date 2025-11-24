0f49716d4437f73ce1c03062d936b69040592d1d
FurkanHuman <furkan-b136@hotmail.com>
2025-11-24 03:47:52 +0300

    refactor(Analysis): remove AnalysisLessonPassingScoreCalculator and implement GetByIdAnalysisLessonPassingScoreQuery for passing score retrieval

--- Src/SES/Application/ApplicationServiceRegistration.cs
+++ Src/SES/Application/ApplicationServiceRegistration.cs
@@ -103,7 +103,6 @@ public static class ApplicationServiceRegistration
         services.AddScoped<IAnalysisCalculatorFactory<AdvancedStatisticsData>, AnalysisAdvancedStatisticsCalculator>();
         services.AddScoped<IAnalysisCalculatorFactory<List<List<StudentPerformanceDto>>>, AnalysisClusteringFactory>();
         services.AddScoped<IAnalysisCalculatorFactory<BenefitAnalysisResultDto>, AnalysisBenefitCalculator>();
-        services.AddScoped<IAnalysisCalculatorFactory<int>, AnalysisLessonPassingScoreCalculator>();
 
         services.AddScoped<AnalysisAutomationService>();
         services.AddScoped<PdfReaderStudentManager>();
---
+++ Src/SES/Application/Features/Analyses/Queries/GetByIdAnalysisLessonPassingScore/GetByIdAnalysisLessonPassingScoreQuery.cs
@@ -0,0 +1,39 @@
+﻿using Application.Features.Analyses.Rules;
+using Application.Services.Repositories;
+using Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Features.Analyses.Queries.GetByIdAnalysisLessonPassingScore;
+
+public class GetByIdAnalysisLessonPassingScoreQuery : IRequest<int>
+{
+    public int Id { get; set; }
+}
+
+public class GetByIdLessonPassingScoreQueryHandler : IRequestHandler<GetByIdAnalysisLessonPassingScoreQuery, int>
+{
+    private readonly IAnalysisRepository _analysisRepository;
+    private readonly AnalysisBusinessRules _analysisBusinessRules;
+
+    public GetByIdLessonPassingScoreQueryHandler(IAnalysisRepository analysisRepository, AnalysisBusinessRules analysisBusinessRules)
+    {
+        _analysisRepository = analysisRepository;
+        _analysisBusinessRules = analysisBusinessRules;
+    }
+
+    public async Task<int> Handle(GetByIdAnalysisLessonPassingScoreQuery request, CancellationToken cancellationToken)
+    {
+        await _analysisBusinessRules.AnalysisIdShouldExistWhenSelected(request.Id, cancellationToken);
+
+        Analysis? analysis = await _analysisRepository.GetAsync(
+                                                                predicate: a => a.Id == request.Id,
+                                                                include: a => a.Include(a => a.Lesson),
+                                                                cancellationToken: cancellationToken
+                                                                );
+
+        await _analysisBusinessRules.AnalysisShouldExistWhenSelected(analysis);
+
+        return analysis!.Lesson.PassingScore;
+    }
+}
