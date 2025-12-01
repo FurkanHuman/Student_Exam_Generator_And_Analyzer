7ac32eefa402b91443182031164cf29a8c736534
FurkanHuman <furkan-b136@hotmail.com>
2025-12-01 17:59:44 +0300

    fix(BlazorWebUIServiceRegistration): The correction of the deleted important service record has been made.

--- Src/SES/BlazorWebUI/BlazorWebUI/BlazorWebUIServiceRegistration.cs
+++ Src/SES/BlazorWebUI/BlazorWebUI/BlazorWebUIServiceRegistration.cs
@@ -14,6 +14,7 @@ public static class BlazorWebUIServiceRegistration
 
         services.AddAutoMapper(cfg => { }, Assembly.GetExecutingAssembly());
         services.AddApexCharts();
+        services.AddScoped<IdentityUserAccessor>();
         services.AddScoped<IdentityRedirectManager>();
         services.AddScoped<AuthenticationStateProvider, PersistingRevalidatingAuthenticationStateProvider>();
         services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
