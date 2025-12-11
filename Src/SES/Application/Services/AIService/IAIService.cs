c8d1bae925a6afd8e48c6e456549c38a81b1d417
FurkanHuman <furkan-b136@hotmail.com>
2025-12-11 16:12:23 +0300

    fix(IAIService): name change, parameter and return type correction

--- Src/SES/Application/Services/AIService/IAIService.cs
+++ Src/SES/Application/Services/AIService/IAIService.cs
@@ -1,8 +1,9 @@
 ﻿using Application.Services.AIService.Models;
 
 namespace Application.Services.AIService;
+
 public interface IAIService
 {
     Task<AIAnalysisResponse> GenerateAnalysisFromAIAsync(AIAnalysisRequest analysisRequest, string aiModel, CancellationToken cancellationToken);
-    Task<ICollection<QuestionAIInComingModel>?> GenerateQuestionsFromAIAsync(QuestionAIOutgoingModel outgoingModel, string aiModel, CancellationToken cancellationToken);
+    Task<List<AIQuestionGenerationResponse>> GenerateQuestionsFromAIAsync(AIQuestionGenerationRequest generationRequest, string aiModel, CancellationToken cancellationToken);
 }
