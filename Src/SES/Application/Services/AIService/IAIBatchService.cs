f7854ba35fbd2b852249dcceaed935701db6b136
FurkanHuman <furkan-b136@hotmail.com>
2025-12-23 03:49:45 +0300

    feat: add IAIBatchService interface for batch analysis functionality and update AI models

---
+++ Src/SES/Application/Services/AIService/IAIBatchService.cs
@@ -0,0 +1,8 @@
+﻿using Application.Services.AIService.Models;
+
+namespace Application.Services.AIService;
+
+public interface IAIBatchService
+{
+    Task<List<AIAnalysisResponse>> GenerateBatchAnalysisAsync(List<AIAnalysisRequest> aIAnalysisRequests, string model, CancellationToken cancellationToken);
+}
